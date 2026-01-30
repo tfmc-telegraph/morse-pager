42956b54994ce76e95b34c484101b94c7c64bccf
oaokaklak <145412149+oaokaklak@users.noreply.github.com>
2026-01-30 13:11:15 -0500

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,17 +1,15 @@
-const Pusher = require("pusher");
+const Ably = require("ably");
 
-const pusher = new Pusher({
-  appId: "YOUR_APP_ID",
-  key: "YOUR_KEY",
-  secret: "YOUR_SECRET",
-  cluster: "YOUR_CLUSTER",
-  useTLS: true
-});
+// Set this variable in Vercel Dashboard
+const rest = new Ably.Rest({ key: process.env.ABLY_SECRET_KEY });
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
   if (req.method === 'POST') {
-    const { to, message } = req.body;
-    await pusher.trigger(to, "new-morse", { text: message });
+    const { to, message } = JSON.parse(req.body);
+    const channel = rest.channels.get(to);
+    await channel.publish('new-morse', { text: message });
     return res.status(200).json({ success: true });
   }
   res.status(405).send("Method Not Allowed");
